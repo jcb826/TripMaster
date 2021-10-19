ba22d59736cc6521d2036be39e5526c0c35ce955
Jean-César <jeancesar.bazin@gmail.com>
2021-10-19 13:15:02 +0200

    first commit

---
+++ TripMaster/src/main/java/com/example/TripMaster/TripMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.TripMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripMasterApplication.class, args);
+	}
+
+}
---
+++ TripMaster/src/test/java/com/example/TripMaster/TripMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
